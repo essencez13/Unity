3ef22e3af4472cd2dc8d33a9644988b751d39ec8
essencez13 <kzzk238@gmail.com>
2026-07-15 18:59:23 +0400

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -8,22 +9,34 @@ public class Player : MonoBehaviour
     private int coins;
     public GameObject fireballPrefab;
     public Transform attackPoint;
+    public AudioSource audioSource;
+    public AudioClip damageSound;
 
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Здоровье игрока: " + health);
+
+        if (health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+            print("Р—РґРѕСЂРѕРІСЊРµ РёРіСЂРѕРєР°: " + health);
+        }
     }
     public void CollectCoins()
     {
         coins++;
-        print("Собранные монетки: " + coins);
+        print("РЎРѕР±СЂР°РЅРЅС‹Рµ РјРѕРЅРµС‚РєРё: " + coins);
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);    
         }
 
     }
