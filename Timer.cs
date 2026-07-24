5b8c5d794380918aff5a322d14054f1a852b25bd
essencez13 <kzzk238@gmail.com>
2026-07-24 19:01:49 +0400

    Timer

---
+++ Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public int minutes;
+    public float seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+        if (seconds <= 0)
+        {
+            if (minutes > 0)
+            {
+                seconds += 59;
+                minutes--;
+            }
+            else
+            {
+                int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+    }
+}
