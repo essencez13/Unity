4aa523d65ae47a4c7ed3bb859ac9c051650b54e6
essencez13 <kzzk238@gmail.com>
2026-07-29 18:57:07 +0400

    Health

---
+++ Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public int health = 10;
+    public int maxHealth = 10;
+    public AudioSource audioSource;
+    public AudioClip damageSound;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+    
+
+    }
+}
