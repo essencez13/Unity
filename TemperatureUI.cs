02eb2e84f286e01a873a9b492cc501b7e333720a
essencez13 <kzzk238@gmail.com>
2026-07-31 19:00:29 +0400

    Add files via upload

---
+++ TemperatureUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TemperatureUI : MonoBehaviour
+{
+    public Temperature temperature;
+    public TextMeshProUGUI temperatureText;
+    
+    void Update()
+    {
+        float roundTemperature = Mathf.Round(temperature.temperatureCurrent * 10.0f) * 0.1f;
+        temperatureText.text = roundTemperature.ToString();
+    }
+}
