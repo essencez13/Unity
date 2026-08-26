ed4702a34799dcbf2d55d8097e7f79299921d650
essencez13 <kzzk238@gmail.com>
2026-08-26 18:58:08 +0400

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,44 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public int health = 10;
-    public int maxHealth = 10;
-    public int coins;
-    public GameObject fireballPrefab;
-    public Transform attackPoint;
-    public AudioSource audioSource;
-    public AudioClip damageSound;
-
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-
-        if (health > 0)
-        {
-            audioSource.PlayOneShot(damageSound);
-        }
-        else
-        {
-            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(sceneIndex);
-            print("Здоровье игрока: " + health);
-        }
-    }
-    public void CollectCoins()
-    {
-        coins++;
-        print("Собранные монетки: " + coins);
-    }
+    public int points;
+    public Projectile projectilePrefab;
+    public float shootInterval;
+    public float shootTimer;
+    public Transform shootPoint;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Move();
+        shootTimer -= Time.deltaTime;
+        Shoot();
+    }
+    void Move()
+    {
+        if (Input.GetMouseButton(0))
         {
-            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);    
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 realPos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = realPos;
         }
-
+    }
+    void Shoot()
+    {
+        if (shootTimer <= 0)
+        {
+            Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+            shootTimer = shootInterval;
+        }
     }
 }
